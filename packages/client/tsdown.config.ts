1740751f943b536d604347276b4cb47fc7500662
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2025-11-17 11:49:28 +0100

    fix: ah publish shared f it

--- packages/client/tsdown.config.ts
+++ packages/client/tsdown.config.ts
@@ -4,7 +4,5 @@ export default defineConfig({
   entry: ['src/index.ts'],
   dts: {
     tsgo: true,
-    resolve: ['@rttnd/llm-shared'], // TODO: doesn't work, it can't inline types, only js is inlined
   },
-  noExternal: ['@rttnd/llm-shared'],
 })
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,4 @@
 export * from './custom'
+export * from './registry'
 export * from './search'
 export * from './types'
